5eda2e3ff63fa59c1916beda32902266ba80d1dd
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-05-05 12:35:05 +0000

    Update examples

--- examples/effect.hs
+++ examples/effect.hs
@@ -2,10 +2,19 @@
 import Data.Extensible
 
 decEffects [d|
-  data Example x where
+  data Example x where -- the name doesn't matter
     Foo :: Int -> Example ()
     Bar :: Example String
     Baz :: Bool -> Bool -> Example Int
     |]
 
 mkField "Foo Bar Baz"
+
+test :: (Associate "Foo" (Action '[Int] ()) xs
+  , Associate "Bar" (Action '[] String) xs
+  , Associate "Baz" (Action '[Bool, Bool] Int) xs) => Eff xs Int
+test = do
+  foo 42
+  s <- bar
+  t <- bar
+  baz (s == "bar") (s == t)
