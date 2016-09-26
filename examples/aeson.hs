91af6523c4abce01f3c88035b58a07a036ac0dad
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-09-26 16:49:45 +0900

    use proxyAssocKey

--- examples/aeson.hs
+++ examples/aeson.hs
@@ -1,16 +1,18 @@
-{-# LANGUAGE TypeOperators, DataKinds, FlexibleContexts, FlexibleInstances, UndecidableInstances, PolyKinds, TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 import Data.Aeson (FromJSON(..), withObject)
-import Data.Extensible (Record, Field(..), KeyValue, AssocKey, Forall, hgenerateFor)
+import Data.Extensible
 import GHC.TypeLits (KnownSymbol, symbolVal)
 import Data.Proxy
 import Data.String (fromString)
 import qualified Data.HashMap.Strict as HM
 
-keyProxy :: proxy kv -> Proxy (AssocKey kv)
-keyProxy _ = Proxy
-
 instance Forall (KeyValue KnownSymbol FromJSON) xs => FromJSON (Record xs) where
   parseJSON = withObject "Object" $ \v -> hgenerateFor (Proxy :: Proxy (KeyValue KnownSymbol FromJSON))
-    $ \m -> let k = symbolVal (keyProxy m) in case HM.lookup (fromString k) v of
+    $ \m -> let k = symbolVal (proxyAssocKey m) in case HM.lookup (fromString k) v of
       Just a -> Field <$> return <$> parseJSON a
       Nothing -> fail $ "Missing key: " ++ k
