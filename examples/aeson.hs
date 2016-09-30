ce0ea16732e7eff1082badec429787f2452353e5
fumieval <fumiexcel@gmail.com>
2016-09-30 20:36:50 +0900

    examples/aeson.hs: Add ToJSON instance

--- examples/aeson.hs
+++ examples/aeson.hs
@@ -2,13 +2,16 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-import Data.Aeson (FromJSON(..), withObject)
+import Data.Aeson hiding (KeyValue)
+import Data.Constraint
 import Data.Extensible
-import GHC.TypeLits (KnownSymbol, symbolVal)
+import Data.Monoid
 import Data.Proxy
 import Data.String (fromString)
+import GHC.TypeLits (KnownSymbol, symbolVal)
 import qualified Data.HashMap.Strict as HM
 
 instance Forall (KeyValue KnownSymbol FromJSON) xs => FromJSON (Record xs) where
@@ -16,3 +19,9 @@ instance Forall (KeyValue KnownSymbol FromJSON) xs => FromJSON (Record xs) where
     $ \m -> let k = symbolVal (proxyAssocKey m) in case HM.lookup (fromString k) v of
       Just a -> Field <$> return <$> parseJSON a
       Nothing -> fail $ "Missing key: " ++ k
+
+instance Forall (KeyValue KnownSymbol ToJSON) xs => ToJSON (Record xs) where
+  toJSON rec = Object $ HM.fromList $ flip appEndo [] $ hfoldMap getConst'
+    $ hzipWith (\(Comp Dict) v -> Const' $ Endo
+      ((fromString $ symbolVal $ proxyAssocKey v, toJSON $ getField v):))
+    (library :: Comp Dict (KeyValue KnownSymbol ToJSON) :* xs) rec
