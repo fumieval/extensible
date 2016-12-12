eef9fd34f35740ca83dddabaa62a952d435123ba
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-12-12 15:49:42 +0900

    generalise Data.Extensible.Tangle further

--- src/Data/Extensible/Tangle.hs
+++ src/Data/Extensible/Tangle.hs
@@ -10,39 +10,35 @@ import Data.Extensible.Internal.Rig
 import Data.Extensible.Nullable
 import Data.Extensible.Wrapper
 
--- | @'TangleT' h xs m@ is the monad of computations that may depend on the fields in 'xs'.
+-- | @'TangleT' h xs m@ is the monad of computations that may depend on the elements in 'xs'.
 newtype TangleT h xs m a = TangleT
-    { unTangleT :: RWST
-        (RecordOf (Comp (TangleT h xs m) h) xs)
-        ()
-        (RecordOf (Nullable h) xs)
-        m a }
+    { unTangleT :: RWST (Comp (TangleT h xs m) h :* xs) () (Nullable h :* xs) m a }
     deriving (Functor, Applicative, Monad)
 
 instance MonadTrans (TangleT h xs) where
     lift = TangleT . lift
 
--- | Get a value of the specified field.
-hitch :: forall k v m h xs. (Monad m, Associate k v xs) => FieldName k -> TangleT h xs m (h v)
-hitch _ = hitchAt (association :: Membership xs (k ':> v))
+-- | Hitch an element associated to the 'FieldName' through a wrapper.
+lasso :: forall k v m h xs. (Monad m, Associate k v xs, Wrapper h)
+    => FieldName k -> TangleT h xs m (Repr h (k ':> v))
+lasso _ = view _Wrapper <$> hitchAt (association :: Membership xs (k ':> v))
 
 -- | Take a value from the tangles. The result is memoized.
-hitchAt :: Monad m => Membership xs kv -> TangleT h xs m (h (AssocValue kv))
+hitchAt :: Monad m => Membership xs x -> TangleT h xs m (h x)
 hitchAt k = TangleT $ do
     mem <- get
-    case getNullable $ getField $ hlookup k mem of
+    case getNullable $ hlookup k mem of
         Just a -> return a
         Nothing -> do
             tangles <- ask
-            a <- unTangleT $ getComp $ getField $ hlookup k tangles
-            modify $ over (pieceAt k) $ const $ Field $ Nullable $ Just a
+            a <- unTangleT $ getComp $ hlookup k tangles
+            modify $ over (pieceAt k) $ const $ Nullable $ Just a
             return a
 
 -- | Run tangles and collect the results as a 'Record'.
 runTangles :: Monad m
-    => RecordOf (Comp (TangleT h xs m) h) xs -- ^ tangle matrix
-    -> RecordOf (Nullable h) xs -- ^ pre-calculated values
-    -> m (RecordOf h xs)
+    => Comp (TangleT h xs m) h :* xs -- ^ tangle matrix
+    -> Nullable h :* xs -- ^ pre-calculated values
+    -> m (h :* xs)
 runTangles tangles rec0 = fst <$> evalRWST
-    (unTangleT $ htraverseWithIndex (\k _ -> Field <$> hitchAt k) rec0)
-    tangles rec0
+    (unTangleT $ htraverseWithIndex (const . hitchAt) rec0) tangles rec0
