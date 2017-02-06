47024f0c0f284904a466db574dcd35b943ad36cb
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-02-06 16:00:03 +0900

    Data.Extensible.Effect: Brush up

--- src/Data/Extensible/Effect.hs
+++ src/Data/Extensible/Effect.hs
@@ -1,47 +1,34 @@
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances, UndecidableInstances #-}
-module Data.Extensible.Effect (Instruction(..)
+{-# LANGUAGE TypeFamilies #-}
+module Data.Extensible.Effect (
+  -- * Base
+  Instruction(..)
   , Eff
   , liftEff
   , hoistEff
-  , handleWith
+  , handleEff
+  , peelEff
   , Handler(..)
-  -- * Unnamed actions
+  -- * Anonymous actions
   , Action(..)
   , Function
-  , receive
-  -- * Successive handling
-  , (!-!!)
-  , squash
-  , nihility) where
+  , runAction
+  , (@!?)
+  -- * transformers-compatible handlers
+  , runReaderAs
+  , runStateAs
+  , runWriterAs
+  , runMaybeAs
+  , runEitherAs
+  ) where
 
 import Control.Monad.Skeleton
+import Control.Monad.Trans.State.Strict
 import Data.Extensible.Field
 import Data.Extensible.Internal
 import Data.Extensible.Internal.Rig
 import Data.Extensible.Class
 import Data.Profunctor.Unsafe -- Trustworthy since 7.8
-
--- | Unnamed action
-data Action (args :: [*]) a r where
-  AResult :: Action '[] a a
-  AArgument :: x -> Action xs a r -> Action (x ': xs) a r
-
-type family Function args r :: * where
-  Function '[] r = r
-  Function (x ': xs) r = x -> Function xs r
-
--- | Transformation between effects
-newtype Handler f g = Handler { runHandler :: forall a. g a -> f a }
-
-receive :: Functor f => Function xs (f a) -> Handler f (Action xs a)
-receive f0 = Handler (go f0) where
-  go :: Functor f => Function xs (f a) -> Action xs a r -> f r
-  go r AResult = r
-  go f (AArgument x a) = go (f x) a
-
-----------------------------------------------
 
 -- | A unit of effects
 data Instruction (xs :: [Assoc k (* -> *)]) a where
@@ -61,35 +48,81 @@ hoistEff _ f = hoistSkeleton $ \(Instruction i t) -> case compareMembership (ass
   _ -> Instruction i t
 {-# INLINABLE hoistEff #-}
 
-handleWith :: RecordOf (Handler m) xs -> Eff xs a -> MonadView m (Eff xs) a
-handleWith hs m = case unbone m of
+-- | Build a relay-style handler from a triple of functions.
+--
+-- @
+-- runStateAs = peelEff (\a s -> return (a, s))
+--   (\m k s -> let (a, s') = runState m s in k a s')
+--   (\i k s -> boned (i :>>= flip k s))
+-- @
+--
+peelEff :: (a -> r) -- return the result
+  -> (forall x. t x -> (x -> r) -> r) -- ^ Handle the foremost type of an action
+  -> (forall x. Instruction xs x -> (x -> r) -> r) -- ^ Re-bind an unrelated action
+  -> proxy k -> Eff (k >: t ': xs) a -> r
+peelEff ret wrap pass _ = go where
+  go m = case unbone m of
+    Return a -> ret a
+    Instruction i t :>>= k -> runMembership i
+      (\Refl -> wrap t (go . k))
+      (\j -> pass (Instruction j t) (go . k))
+{-# INLINE peelEff #-}
+
+-- | Transformation between effects
+newtype Handler f g = Handler { runHandler :: forall a. g a -> f a }
+
+-- | Process an 'Eff' action using a record of 'Handler's.
+handleEff :: RecordOf (Handler m) xs -> Eff xs a -> MonadView m (Eff xs) a
+handleEff hs m = case unbone m of
   Instruction i t :>>= k -> views (pieceAt i) (runHandler .# getField) hs t :>>= k
   Return a -> Return a
-{-# INLINABLE handleWith #-}
 
-(!-!!) :: Monad m => (forall x. t x -> m x)
-  -> (forall x. Eff xs x -> m x)
-  -> Eff ((s ':> t) ': xs) a -> m a
-f !-!! g = go where
-  go m = case unbone m of
-    Return a -> return a
-    Instruction i t :>>= k -> runMembership i
-      (\Refl -> f t >>= go . k)
-      (\j -> g (bone (Instruction j t)) >>= go . k)
-{-# INLINE (!-!!) #-}
-infixr 0 !-!!
+-- | A heterogeneous list of arguments
+data Action (args :: [*]) a r where
+  AResult :: Action '[] a a
+  AArgument :: x -> Action xs a r -> Action (x ': xs) a r
 
-nihility :: Monad m => Eff '[] a -> m a
-nihility m = case unbone m of
-  Return a -> return a
-  Instruction i _ :>>= _ -> impossibleMembership i
+-- | @'Function' [a, b, c] r@ is @a -> b -> c -> r@
+type family Function args r :: * where
+  Function '[] r = r
+  Function (x ': xs) r = x -> Function xs r
 
--- | @'squash' = ('!-!!' 'id')@
-squash :: (forall x. t x -> Eff xs x) -> Eff ((s ':> t) ': xs) a -> Eff xs a
-squash f = go where
-  go m = case unbone m of
-    Return a -> return a
-    Instruction i t :>>= k -> runMembership i
-      (\Refl -> f t >>= go . k)
-      (\j -> boned $ Instruction j t :>>= go . k)
-{-# INLINE squash #-}
+-- | Pass the arguments of 'Action' to the supplied function.
+runAction :: Function xs (f a) -> Action xs a r -> f r
+runAction r AResult = r
+runAction f (AArgument x a) = runAction (f x) a
+
+-- | Create a 'Field' of a 'Handler' for an 'Action'.
+(@!?) :: FieldName k -> Function xs (f a) -> Field (Handler f) (k ':> Action xs a)
+_ @!? f = Field $ Handler (runAction f)
+infix 1 @!?
+
+runReaderAs :: proxy k -> Eff (k >: (->) r ': xs) a -> r -> Eff xs a
+runReaderAs = peelEff (\a _ -> return a)
+  (\m k r -> k (m r) r)
+  (\i k r -> boned (i :>>= flip k r))
+{-# INLINE runReaderAs #-}
+
+runStateAs :: proxy k -> Eff (k >: State s ': xs) a -> s -> Eff xs (a, s)
+runStateAs = peelEff (\a s -> return (a, s))
+  (\m k s -> let (a, s') = runState m s in k a s')
+  (\i k s -> boned (i :>>= flip k s))
+{-# INLINE runStateAs #-}
+
+runWriterAs :: Monoid w => proxy k -> Eff (k >: (,) w ': xs) a -> Eff xs (a, w)
+runWriterAs p m0 = peelEff (\a w -> return (a, w))
+  (\(w', a) k w -> k a $! mappend w w')
+  (\i k w -> boned (i :>>= flip k w)) p m0 mempty
+{-# INLINE runWriterAs #-}
+
+runMaybeAs :: proxy k -> Eff (k >: Maybe ': xs) a -> Eff xs (Maybe a)
+runMaybeAs = peelEff (return . Just)
+  (\m k -> maybe (return Nothing) k m)
+  (\i k -> boned (i :>>= k))
+{-# INLINE runMaybeAs #-}
+
+runEitherAs :: proxy k -> Eff (k >: Either e ': xs) a -> Eff xs (Either e a)
+runEitherAs = peelEff (return . Right)
+  (\m k -> either (return . Left) k m)
+  (\i k -> boned (i :>>= k))
+{-# INLINE runEitherAs #-}
