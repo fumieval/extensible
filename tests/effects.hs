a5b7c29903c7516e35b2b4b89cba7026d0d6331d
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-02-08 19:11:52 +0900

    WIP: decEffects is broken

---
+++ tests/effects.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE GADTs, DataKinds, TemplateHaskell #-}
+{-# OPTIONS_GHC -ddump-splices #-}
+import Data.Extensible
+
+decEffects [d|
+  data Example a b x where
+    Concrete :: Int -> Example a b ()
+    PolyArg :: a -> Example a b ()
+    PolyRes :: Example a b b
+    PolyArgRes :: a -> Example a b b
+    ExtArg :: Show s => s -> Example a b ()
+    ExtRes :: Read s => Example a b s
+  |]
+
+main = ()
