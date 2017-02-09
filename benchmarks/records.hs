e33b862bd234aa354a9fd674e537bced657c7bd8
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-02-09 18:23:08 +0900

    more benchmarks

--- benchmarks/records.hs
+++ benchmarks/records.hs
@@ -4,7 +4,7 @@ import Control.Lens
 import Data.Extensible
 import Data.Monoid
 
-mkField "foo bar baz qux foobar foobaz fooqux"
+mkField "foo bar baz qux foobar foobaz fooqux barfoo barbaz barqux"
 
 type Fields = ["foo" >: Sum Int
   , "bar" >: String
@@ -12,13 +12,19 @@ type Fields = ["foo" >: Sum Int
   , "qux" >: String
   , "foobar" >: (Sum Int, String)
   , "foobaz" >: (Sum Int, First Int)
-  , "fooqux" >: (Sum Int, String)]
+  , "fooqux" >: (Sum Int, String)
+  , "barfoo" >: (String, Sum Int)
+  , "barbaz" >: (String, First Int)
+  , "barqux" >: (String, String)]
 
 recA :: Record Fields
 recA = foo @= Sum 1 <: bar @= "barA" <: baz @= mempty <: qux @= "qux"
     <: foobar @= (Sum 1, "foobar")
     <: foobaz @= (Sum 5, mempty)
     <: fooqux @= (Sum 6, mempty)
+    <: barfoo @= mempty
+    <: barbaz @= mempty
+    <: barqux @= mempty
     <: nil
 {-# NOINLINE recA #-}
 
@@ -27,28 +33,39 @@ recB = foo @= Sum 2 <: bar @= "barB" <: baz @= pure 42 <: qux @= "qux"
   <: foobar @= (Sum 1, "foobar")
   <: foobaz @= (Sum 5, mempty)
   <: fooqux @= (Sum 7, mempty)
+  <: barfoo @= mempty
+  <: barbaz @= mempty
+  <: barqux @= mempty
   <: nil
 {-# NOINLINE recB #-}
 
-data HsRec = HsRec { hsFoo :: !(Sum Int), hsBar :: !String, hsBaz :: !(First Int)
-  , hsQux :: !String
-  , hsFooBar :: !(Sum Int, String)
-  , hsFooBaz :: !(Sum Int, First Int)
-  , hsFooQux :: !(Sum Int, String)
+data HsRec = HsRec { _hsFoo :: !(Sum Int), _hsBar :: !String, _hsBaz :: !(First Int)
+  , _hsQux :: !String
+  , _hsFooBar :: !(Sum Int, String)
+  , _hsFooBaz :: !(Sum Int, First Int)
+  , _hsFooQux :: !(Sum Int, String)
+  , _hsBarFoo :: !(String, Sum Int)
+  , _hsBarBaz :: !(String, First Int)
+  , _hsBarQux :: !(String, String)
   }
+makeLenses ''HsRec
 
-hsRec = HsRec { hsFoo = Sum 1, hsBar = "hsBar"
-  , hsBaz = mempty, hsQux = "hsQux"
-  , hsFooBar = (Sum 1, "foobar")
-  , hsFooBaz = (Sum 5, mempty)
-  , hsFooQux = (Sum 6, mempty)
+hsRec = HsRec { _hsFoo = Sum 1, _hsBar = "hsBar"
+  , _hsBaz = mempty, _hsQux = "hsQux"
+  , _hsFooBar = (Sum 1, "foobar")
+  , _hsFooBaz = (Sum 5, mempty)
+  , _hsFooQux = (Sum 6, mempty)
+  , _hsBarFoo = mempty
+  , _hsBarBaz = mempty
+  , _hsBarQux = mempty
   }
 
 main = defaultMain
   [ bgroup "basic"
     [ bench "view" $ whnf (view foo) recA
+    , bench "hsview" $ whnf (view hsFoo) hsRec
     , bench "set" $ whnf (set foo 3) recB
-    , bench "hsset" $ whnf (\r -> r { hsFoo = 3}) hsRec
+    , bench "hsset" $ whnf (set hsFoo 3) hsRec
     ]
   , bgroup "instances"
     [ bench "mappend" $ whnf (uncurry mappend) (recA, recB)
