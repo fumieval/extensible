885aa3870f88653540fffe79f4eca20e6a4ed04f
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-10-29 16:58:46 +0900

    Data.Extensible.GetOpt: generalise

--- src/Data/Extensible/GetOpt.hs
+++ src/Data/Extensible/GetOpt.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, LambdaCase #-}
+{-# LANGUAGE TypeFamilies, LambdaCase, DeriveFunctor, StandaloneDeriving #-}
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Extensible.GetOpt
@@ -9,9 +9,12 @@
 --
 -- A wrapper for 'System.Console.GetOpt'
 --------------------------------------------------------------------------------
-module Data.Extensible.GetOpt (OptDescr'(..)
+module Data.Extensible.GetOpt (OptionDescr(..)
+  , OptDescr'
   , optNoArg
   , optReqArg
+  , optionNoArg
+  , optionReqArg
   , getOptRecord
   , withGetOpt) where
 
@@ -21,6 +24,7 @@ import Data.Extensible.Field
 import Data.Extensible.Internal.Rig
 import Data.Extensible.Product
 import Data.Extensible.Wrapper
+import Data.Functor.Identity
 import Data.List (foldl')
 import System.Console.GetOpt
 import System.Environment
@@ -28,10 +32,26 @@ import System.Exit
 import System.IO
 
 -- | 'OptDescr' with a default
-data OptDescr' a = OptDescr' a (OptDescr (a -> a))
+data OptionDescr h a = forall s. OptionDescr (s -> h a) !s (OptDescr (s -> s))
 
-instance Wrapper OptDescr' where
-  type Repr OptDescr' a = OptDescr' a
+deriving instance Functor h => Functor (OptionDescr h)
+
+supplyOption :: Maybe String -> OptionDescr h a -> OptionDescr h a
+supplyOption str od@(OptionDescr k s opt@(Option _ _ arg _)) = case (str, arg) of
+  (Just a, ReqArg f _) -> OptionDescr k (f a s) opt
+  (Nothing, NoArg f) -> OptionDescr k (f s) opt
+  (a, OptArg f _) -> OptionDescr k (f a s) opt
+  _ -> od
+
+extendArg :: (Maybe String -> a -> b) -> ArgDescr a -> ArgDescr b
+extendArg f (NoArg a) = NoArg $ f Nothing a
+extendArg f (ReqArg a ph) = ReqArg (\s -> f (Just s) (a s)) ph
+extendArg f (OptArg a ph) = OptArg (f <*> a) ph
+
+type OptDescr' = OptionDescr Identity
+
+instance Wrapper (OptionDescr h) where
+  type Repr (OptionDescr h) a = OptionDescr h a
   _Wrapper = id
 
 -- | Option without an argument; the result is the total count of this option.
@@ -39,7 +59,10 @@ optNoArg :: [Char] -- ^ short option
     -> [String] -- ^ long option
     -> String -- ^ explanation
     -> OptDescr' Int
-optNoArg ss ls expl = OptDescr' 0 $ Option ss ls (NoArg (+1)) expl
+optNoArg = optionNoArg Identity
+
+optionNoArg :: (Int -> h a) -> [Char] -> [String] -> String -> OptionDescr h a
+optionNoArg f ss ls expl = OptionDescr f 0 $ Option ss ls (NoArg (+1)) expl
 
 -- | Option with an argument
 optReqArg :: [Char] -- ^ short option
@@ -47,23 +70,27 @@ optReqArg :: [Char] -- ^ short option
     -> String -- ^ placeholder
     -> String -- ^ explanation
     -> OptDescr' [String]
-optReqArg ss ls ph expl = OptDescr' [] $ Option ss ls (ReqArg (:) ph) expl
+optReqArg = optionReqArg Identity
 
-getOptRecord :: RecordOf OptDescr' xs -- ^ a record of option descriptors
+optionReqArg :: ([String] -> h a) -> [Char] -> [String] -> String -> String -> OptionDescr h a
+optionReqArg f ss ls ph expl = OptionDescr f [] $ Option ss ls (ReqArg (:) ph) expl
+
+getOptRecord :: RecordOf (OptionDescr h) xs -- ^ a record of option descriptors
     -> [String] -- ^ arguments
-    -> (Record xs, [String], [String], String -> String) -- ^ (result, remaining non-options, errors, usageInfo)
-getOptRecord descs args = (foldl' (flip id) def fs, rs, es, flip usageInfo updaters) where
+    -> (RecordOf h xs, [String], [String], String -> String) -- ^ (result, remaining non-options, errors, usageInfo)
+getOptRecord descs args = (result, rs, es, flip usageInfo updaters) where
   (fs, rs, es) = getOpt Permute updaters args
   updaters = hfoldrWithIndex
-      (\i (Field (OptDescr' _ opt)) -> (:)
-          $ fmap (\f -> over (pieceAt i) (Field . fmap f . getField)) opt)
+      (\i (Field (OptionDescr _ _ (Option ss ls arg expl))) -> (:)
+          $ Option ss ls (extendArg (\a _ -> over (pieceAt i) (liftField (supplyOption a))) arg) expl)
       [] descs
-  def = hmap (\(Field (OptDescr' x _)) -> Field (pure x)) descs
+  result = hmap (\(Field (OptionDescr k x _)) -> Field (k x))
+      $ foldl' (flip id) descs fs
 
 -- | When there's an error, print it along with the usage info to stderr
 -- and terminate with 'exitFailure'.
-withGetOpt :: MonadIO m => RecordOf OptDescr' xs
-  -> (Record xs -> [String] -> m a) -> m a
+withGetOpt :: MonadIO m => RecordOf (OptionDescr h) xs
+  -> (RecordOf h xs -> [String] -> m a) -> m a
 withGetOpt descs k = getOptRecord descs <$> liftIO getArgs >>= \case
   (r, xs, [], _) -> k r xs
   (_, _, errs, usage) -> liftIO $ do
