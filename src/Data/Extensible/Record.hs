9878023d70a4df059e9d1c2b9530f77452987406
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-05-31 19:10:39 +0900

    IsRecord via generics

--- src/Data/Extensible/Record.hs
+++ src/Data/Extensible/Record.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE LambdaCase, TemplateHaskell, TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 ------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Extensible.Record
@@ -17,6 +21,7 @@ import Data.Extensible.Product
 import Data.Extensible.Field
 import Data.Functor.Identity
 import Data.Profunctor
+import GHC.Generics
 import GHC.TypeLits
 import Type.Membership
 import Type.Membership.HList
@@ -27,10 +32,51 @@ class IsRecord a where
   recordFromList :: HList (Field Identity) (RecFields a) -> a
   recordToList :: a -> HList (Field Identity) (RecFields a)
 
+  type RecFields a = GRecFields (Rep a) '[]
+  default recordFromList :: (Generic a, GIsRecord (Rep a) '[], GRecFields (Rep a) '[] ~ RecFields a) => HList (Field Identity) (RecFields a) -> a
+  recordFromList xs = recordFromList' xs (\x (HNil :: HList (Field Identity) '[]) -> to x)
+
+  default recordToList :: (Generic a, GIsRecord (Rep a) '[], GRecFields (Rep a) '[] ~ RecFields a) => a -> HList (Field Identity) (RecFields a)
+  recordToList x = recordToList' (from x) HNil
+
 instance IsRecord () where
   type RecFields () = '[]
   recordFromList _ = ()
   recordToList _ = HNil
+
+-- | The class of types that can be converted to/from a 'Record'.
+class GIsRecord f r where
+  type GRecFields f (r :: [Assoc Symbol *]) :: [Assoc Symbol *]
+  recordFromList' :: HList (Field Identity) (GRecFields f r) -> (f x -> HList (Field Identity) r -> cont) -> cont
+  recordToList' :: f x -> HList (Field Identity) r -> HList (Field Identity) (GRecFields f r)
+
+instance (GIsRecord f (GRecFields g r), GIsRecord g r) => GIsRecord (f :*: g) r where
+  type GRecFields (f :*: g) r = GRecFields f (GRecFields g r)
+  recordFromList' xs cont = recordFromList' xs $ \l ys -> recordFromList' ys $ \r zs -> cont (l :*: r) zs
+  {-# INLINE recordFromList' #-}
+  recordToList' (f :*: g) = recordToList' f . recordToList' g
+  {-# INLINE recordToList' #-}
+
+instance GIsRecord (S1 ('MetaSel ('Just name) su ss ds) (K1 i a)) r where
+  type GRecFields (S1 ('MetaSel ('Just name) su ss ds) (K1 i a)) r = (name >: a) ': r
+  recordFromList' (HCons (Field (Identity a)) xs) cont = cont (M1 (K1 a)) xs
+  {-# INLINE recordFromList' #-}
+  recordToList' (M1 (K1 a)) = HCons (Field (Identity a))
+  {-# INLINE recordToList' #-}
+
+instance GIsRecord f r => GIsRecord (C1 i f) r where
+  type GRecFields (C1 i f) r = GRecFields f r
+  recordFromList' xs cont = recordFromList' xs $ cont . M1
+  {-# INLINE recordFromList' #-}
+  recordToList' (M1 f) = recordToList' f
+  {-# INLINE recordToList' #-}
+
+instance GIsRecord f r => GIsRecord (D1 i f) r where
+  type GRecFields (D1 i f) r = GRecFields f r
+  recordFromList' xs cont = recordFromList' xs $ cont . M1
+  {-# INLINE recordFromList' #-}
+  recordToList' (M1 f) = recordToList' f
+  {-# INLINE recordToList' #-}
 
 -- | Convert a value into a 'Record'.
 toRecord :: IsRecord a => a -> Record (RecFields a)
@@ -52,6 +98,7 @@ tvName :: TyVarBndr -> Name
 tvName (PlainTV n) = n
 tvName (KindedTV n _) = n
 
+{-# DEPRECATED deriveIsRecord "Use the generic default methods instead" #-}
 -- | Create an 'IsRecord' instance for a normal record declaration.
 deriveIsRecord :: Name -> DecsQ
 deriveIsRecord name = reify name >>= \case
