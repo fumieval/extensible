912dff399c597b7ab7f3c4aebd16ed85fe063d28
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2019-06-23 20:46:18 +0900

    add example for fromNullable

---
+++ examples/nullable.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DataKinds, TypeOperators, OverloadedLabels, OverloadedStrings #-}
+import Control.Lens
+import qualified Data.Aeson as J
+import Data.Extensible
+import Data.Maybe (fromMaybe)
+
+type ConfigRec = '[ "columns" >: Int, "language_extensions" >: [String] ]
+
+defaultConfig :: Record ConfigRec
+defaultConfig = #columns @= 80 <: #language_extensions @= [] <: nil
+
+main :: IO ()
+main = do
+  config <- fromNullable defaultConfig <$> readConfig "path/to/config.json"
+  putStrLn $ "columns: " ++ (show $ config ^. #columns)
+  putStrLn $ "language_extensions: " ++ (show $ config ^. #language_extensions)
+
+-- dummy
+readConfig :: FilePath -> IO (Nullable (Field Identity) :* ConfigRec)
+readConfig _path = pure $ fromMaybe vacancy (J.decode "{\"columns\": 100}")
